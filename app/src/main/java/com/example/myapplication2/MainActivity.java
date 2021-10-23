95d10fd7b657e7d3abde977a40256d72fc7709de
rlagusdn04 <rlagusdn2004@gmail.com>
2021-10-23 13:33:41 +0900

    text recognize in image

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,19 +1,15 @@
 package com.example.myapplication2;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.myapplication2.databinding.ActivityMainBinding;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         captureBtn = findViewById(R.id.idBtnCapture);
+        Button btn;
 
+
+        ImageView gif_img = (ImageView)findViewById(R.id.gif_img);
+        Glide.with(this).load(R.raw.hash).into(gif_img);
+
+
+
+        btn = (Button)findViewById(R.id.idBtnIMAGE);
         captureBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,7 +37,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                // 갤러리 열기
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
+                startActivity(mIntent);
+
+            }
+        });
     }
 
 }
